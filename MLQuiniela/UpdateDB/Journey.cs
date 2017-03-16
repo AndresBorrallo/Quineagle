0a6ed688409d2429708d3d9f9d2e8d1dcdf3dfef
Jaime Renedo <jrenedo@greeneaglesolutions.com>
2017-03-16 15:46:24 +0100

    Files for updateDB

---
+++ MLQuiniela/UpdateDB/Journey.cs
@@ -0,0 +1,10 @@
+﻿namespace UpdateDB
+{
+    public class Journey
+    {
+        public int number { get; set; }
+        public int first_div { get; set; }
+        public int second_div { get; set; }
+
+    }
+}
