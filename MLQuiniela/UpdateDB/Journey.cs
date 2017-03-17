45c5a6a9450389f568fa397c98ff5ed798218e65
Jaime Renedo <jaimerenedomauduit@gmail.com>
2017-03-17 22:13:30 +0100

    updateDB finished v1.0

--- MLQuiniela/UpdateDB/Journey.cs
+++ MLQuiniela/UpdateDB/Journey.cs
@@ -2,9 +2,9 @@
 {
     public class Journey
     {
-        public int? number_journey { get; set; }
+        public int number_journey { get; set; }
         public int? first_div { get; set; }
         public int? second_div { get; set; }
-
+        public int season { get; set; }
     }
 }
--- MLQuiniela/UpdateDB/Program.cs
+++ MLQuiniela/UpdateDB/Program.cs
@@ -8,34 +8,38 @@ using libQuinEagle;
 using System.Data.SQLite;
 using libQuinEagle.Clasification;
 using System.Collections.Generic;
+using System.Linq;
+using libQuinEagle.Fixtures;
+using Newtonsoft.Json.Linq;
 
 namespace UpdateDB
 {
     class Program
     {
+        // Información de las jornadas sacadas de
+        // http://quinielamania.blogspot.com.es/p/calendario-quiniela.html
+        
+
         static public ILog Log { get; } = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static public string cadenaURL = "https://www.quinielista.es/ayudas/module_help_other_upcoming.asp?num_jornada=$JOURNEY$&num_temporada=$SEASON$";
 
         static void Main(string[] args)
         {
+            // Log4Net
             InitializeLog4net();
-
-            // Cargamos la configuracion
-            Log.Info("Cargando configuracion");
-            Configuration configuration = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(@"./Configuration.json"));
-
+             
             // Cargamos desde base de datos la configuración de las jornadas
             List<Journey> journeys = _getJourneysFromDB();
 
-            // Para cada jornada, cargamos la clasificación y los resultados
+            // Cargamos los equipos
+            _loadTeams();
 
-            foreach(Journey j in journeys){
-                _loadFirstDiv();
-                _loadSecondDiv();
-                _processJourney();
-            };
-
-            _insert2DB();
-                        
+            // Recorremos las jornadas obteniendo los resultados
+            foreach (Journey j in journeys)
+            {
+                _loadAndSaveJourneyResult(j);
+            } 
+                       
             Console.ReadKey();
         }
 
@@ -43,67 +47,114 @@ namespace UpdateDB
         {
             SQLiteConnection conexion = new SQLiteConnection("Data Source=../../../db/quineagle.db;Version=3;New=True;Compress=True;");
             conexion.Open();
-
+            _deleteDB(conexion);
             string consulta = "select * from t_journey";
             SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
             SQLiteDataReader data = cmd.ExecuteReader();
             List<Journey> journeys = new List<Journey>();
 
             Console.WriteLine($"Jornadas encontradas a procesar:");
-            Console.WriteLine($"--------------------------------\n");
+            Console.WriteLine($"--------------------------------");
 
             // Leemos los datos de forma repetitiva
             while (data.Read())
             {
-                int? nj = null, fd = null, sd = null;
+                int nj =0, sea = 0;
                 
                 if (data[1] != System.DBNull.Value)
                     nj = Convert.ToInt16(data[1]);
-
-                if (data[2] != System.DBNull.Value)
-                    fd = Convert.ToInt16(data[2]);
-
-                if (data[3] != System.DBNull.Value)
-                    sd = Convert.ToInt16(data[3]);
+                
+                if (data[4] != System.DBNull.Value)
+                    sea = Convert.ToInt16(data[4]);
 
                 Journey j = new Journey()
                 {
                     number_journey = nj,
-                    first_div = fd,
-                    second_div = sd
+                    season = sea,
                 };
 
                 journeys.Add(j);
 
                 // Y los mostramos                
-                Console.WriteLine($"Jornada número: {nj}, Primera División: {fd}, Segunda División: {sd}");
+                Console.Write($"Jornada número: {nj}");
+
+                Console.Write($"\tTemporada: {sea}\n");
             }
+
+            conexion.Close();
+            Console.WriteLine();
 
             return journeys;
         }
 
-        private static void _loadFirstDiv()
+        private static void _loadAndSaveJourneyResult(Journey j)
         {
-            /*ApiRequester ar = new ApiRequester()
+            int journey = j.number_journey;
+            int season = j.season;
+
+            FixtureRequester fr = new FixtureRequester() {
+                QuinielaFixtureURL = "https://www.quinielista.es/ayudas/module_help_other_upcoming.asp?num_jornada=" + journey + "&num_temporada=" + season
+            };
+
+            fr.LoadFixtures();            
+            List<Fixture> fixtures = fr.GetFixtures();
+
+            var teams = Teams.TeamsNames.Select(x => x.Key);
+            var utilMatches = fixtures.Where(a => a.Result != QuinielaResult.VOID && teams.Contains(a.HomeTeam) && teams.Contains(a.AwayTeam));
+
+            SQLiteConnection conexion = new SQLiteConnection("Data Source=../../../db/quineagle.db;Version=3;New=True;Compress=True;");
+            conexion.Open();
+
+            string consulta = "INSERT INTO `t_match` (id_homeTeam,id_awayTeam,result,id_journey) VALUES ";
+            List<Journey> journeys = new List<Journey>();
+            foreach (var f in utilMatches)
             {
-                API_KEY = configuration.API_KEY,
-                API_URL = configuration.API_URL,
-                RequestHeader = configuration.RequestHeader,
-                LeagueRequest = configuration.LeagueRequest
-            };*/
+                consulta += "( (SELECT id_team from `t_team` where name = '" + f.HomeTeam + "'), "
+                          + "  (SELECT id_team from `t_team` where name = '" + f.AwayTeam + "'), "
+                          + "'" + _parseResult(f.Result) + "',"
+                          + "(SELECT id_journey from `t_journey` where number_journey = '" + j.number_journey + "' and season = '" + j.season + "')),";                
+                Console.WriteLine($"Jornada {j.number_journey}: {f.HomeTeam} - {f.AwayTeam} : {f.Result}");
+            }
+            consulta = consulta.TrimEnd(',');
+            if (utilMatches.Count() > 0)
+            {
+                SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
+                Console.Write(cmd.ExecuteNonQuery() + " rows inserted");
+            }
+            conexion.Close();
         }
 
-        private static void _loadSecondDiv()
+        private static void _deleteDB(SQLiteConnection conexion)
         {
-
+            string consulta = "delete from t_match";
+            SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
+            cmd.ExecuteNonQuery();
         }
 
-        private static void _processJourney()
+        private static char _parseResult(QuinielaResult res)
         {
+            char r;
+            if (res == QuinielaResult.ONE)
+                r = '1';
+            else if (res == QuinielaResult.X)
+                r = 'X';
+            else if (res == QuinielaResult.TWO)
+                r = '2';
+            else
+                r = 'E';
 
+            return r;
         }
 
-        private static void _insert2DB() { }
+        private static void _loadTeams()
+        {
+            // Cargamos nombres de equipos
+            Log.Info("Cargando nombres de equipos de 'TeamsNames.json'");
+            var jObject = JObject.Parse(File.ReadAllText(@"./TeamsNames.json"));
+            var jToken = jObject.GetValue("TeamsNames");
+            Teams.TeamsNames = (Dictionary<string, List<string>>)jToken.ToObject(typeof(Dictionary<string, List<string>>));
+            Log.Info($"Se han cargado {Teams.TeamsNames.Values.Sum(x => x.Count) } nombres de equipos");
+        }
 
         private static void InitializeLog4net()
         {
